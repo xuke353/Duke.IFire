cb25ee2516df3164c722e5ee2f00eaabcc39d1ee
xuke <xuke@sunlight.bz>
2020-12-24 17:28:21 +0800

    🚩

---
+++ IFire.Auth/Web/IFireSession.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IFire.Auth.Abstractions;
+using IFire.Framework.Attributes;
+
+namespace IFire.Auth.Web {
+
+    [Transient]
+    public class IFireSession : IIFireSession {
+    }
+}
