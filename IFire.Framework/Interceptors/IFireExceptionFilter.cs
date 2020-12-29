b41df9cc430236fa16117af2964c5c3db05989ec
xuke <xuke@sunlight.bz>
2020-12-29 13:36:34 +0800

    类库全部更新为.net5

---
+++ IFire.Framework/Interceptors/IFireExceptionFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace IFire.Framework.Interceptors {
+
+    public class IFireExceptionFilter : IExceptionFilter {
+        private readonly ILogger<IFireExceptionFilter> _logger;
+
+        public IFireExceptionFilter(ILogger<IFireExceptionFilter> logger) {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context) {
+        }
+    }
+}
