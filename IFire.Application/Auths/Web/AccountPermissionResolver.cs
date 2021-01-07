546cc4268854c838f3973fd21577aeec63b931ef
xuke <xuke@sunlight.bz>
2021-01-07 11:51:45 +0800

    update

--- IFire.Application/Auths/Web/AccountPermissionResolver.cs
+++ IFire.Application/Auths/Web/AccountPermissionResolver.cs
@@ -2,20 +2,49 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using IFire.Auth.Abstractions;
+using IFire.Domain;
 using IFire.Framework.Attributes;
+using IFire.Framework.Extensions;
 using Microsoft.Extensions.Caching.Distributed;
+using IFire.Domain.RepositoryIntefaces;
+using IFire.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace IFire.Application.Auths.Web {
 
     [Transient]
-    public class AccountPermissionResolver : IFireAppServiceBase, IAccountPermissionResolver {
+    public class AccountPermissionResolver : IAccountPermissionResolver {
         private readonly IDistributedCache _cache;
+        private readonly IRepository<RolePermission, int> _rolePermissionRepository;
+        private readonly IRepository<AccountRole, int> _accountRoleRepository;
 
-        public AccountPermissionResolver(IDistributedCache cache) {
+        public AccountPermissionResolver(IDistributedCache cache,
+            IRepository<RolePermission, int> rolePermissionRepository,
+            IRepository<AccountRole, int> accountRoleRepository) {
             _cache = cache;
+            _rolePermissionRepository = rolePermissionRepository;
+            _accountRoleRepository = accountRoleRepository;
         }
 
         public async Task<IList<string>> Resolve(int userId) {
+            if (userId == 0)
+                return new List<string>();
+
+            //var key = $"{CacheKeys.ACCOUNT_PERMISSIONS}{userId}";
+
+            //if (!_cache.TryGetValue(key, out IList<string> list)) {
+            //    list = await (from rp in _rolePermissionRepository.GetAll()
+            //                  join ar in _accountRoleRepository.GetAll()
+            //                  on rp.RoleId equals ar.RoleId
+            //                  where ar.UserId == userId
+            //                  select rp.PermissionCode)
+            //            .AsNoTracking().ToListAsync();
+            //    await _cache.SetAsync(key, list);
+            //}
+
+            //return list;
+
             var list = new List<string> {
                 "WeatherForecast_Get_Get", "Permission_Tree_Get"
             };
---
+++ IFire.Models/AccountRole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IFire.Framework.Abstractions;
+
+namespace IFire.Models {
+
+    /// <summary>
+    /// 账户角色
+    /// </summary>
+    [Table("Account_Role")]
+    public class AccountRole : Entity<int> {
+
+        /// <summary>
+        /// 账户编号
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// 角色编号
+        /// </summary>
+        public Guid RoleId { get; set; }
+    }
+}
